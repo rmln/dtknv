7cf29a917a2ebe080a0a505fe7e0009606b7dfde
marw <cheesepy@gmail.com>
2011-07-26 20:23:17 +0200

    Updated arg. test files / basic test on Linux passed.

--- test/argtest.sh
+++
@@ -1,5 +0,0 @@
-
-# A file to test dtknv in console.
-cd ..
-cd dtknv
-python3 cknv.py -r -d /home/marw/Documents/testdtknv -o /home/marw/Documents/testout 
---
+++ test/linux_argtest.sh
@@ -0,0 +1,5 @@
+
+# A file to test dtknv in console.
+cd ..
+cd dtknv
+python3 cknv.py -r -i /home/marw/Documents/testdtknv -o /home/marw/Documents/testout 
