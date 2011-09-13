9cef7b15145a364ece8b038cd90bf67dcf146998
marw <mlinar@languagebits.com>
2011-09-13 23:38:20 +0200

    Settings for file extensions.

--- dtknv/gui/window_filesdir.py
+++ dtknv/gui/window_filesdir.py
@@ -16,14 +16,17 @@ class FilesDir:
         self.lng = self.master.lng
         self.set = Set
         self.window = tk.Frame(master, relief='groove', borderwidth=2)
-        self.label = tk.Label(self.window)
-        self.label.pack(anchor='w', padx=5, pady=5)
+        # The label for basic info
+        self.label_selection = tk.Label(self.window)
+        self.label_selection.pack(anchor='w', padx=5, pady=5)
+        # The label about selected file or dir
         self.window.pack(anchor='w', padx=5, pady=8, fill='x')
         self.update_gui()
     
     def update_gui(self):
         """Update label text"""
+        
         text = self.lng['label_file'] + str(self.set.set_file) + '\n' +  \
                       self.lng['label_dir'] + str(self.set.set_dir) + '\n' +  \
                       self.lng['label_dirout'] + str(self.set.set_dirout)
-        self.label.configure(text=text, justify='left')
+        self.label_selection.configure(text=text, justify='left')
