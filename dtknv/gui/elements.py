1c8203888e2d3ab7f1b214c6f6ae61d4a61b7519
marw <mlinar@languagebits.com>
2011-09-21 21:25:10 +0200

    Class for a clicable HTTP link.

--- dtknv/gui/elements.py
+++ dtknv/gui/elements.py
@@ -6,10 +6,30 @@ Various elements for the GUI.
 
 """
 
+import os
+import webbrowser
+
 import tkinter as tk
 from tkinter  import filedialog
+
 import helpers
-import os
+
+
+class Link:
+    """Clicable HTTP link that opens default browser"""
+    def __init__(self, master, text='text', link='localhost',
+                 anchor='w', padx=10, pady=1):
+        self.link = link
+        self.label = tk.Label(master)
+        self.label.configure(text=text, fg='blue', cursor='hand1')
+        self.label.bind('<Button-1>', self.browse)
+        self.label.pack(anchor=anchor, padx=padx, pady=pady)
+
+    def browse(self, *e):
+        """Open browser"""
+        browser = webbrowser.get()
+        browser.open(self.link)
+
 
 class Browse:
     """Browse for file or folder"""
