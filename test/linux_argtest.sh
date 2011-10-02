b0c8d3605a293df1c5015753a1b7083bd6d2e5ff
marw <mlinar@languagebits.com>
2011-10-02 11:57:45 +0200

    Deletions.

--- test/linux_argtest.sh
+++
@@ -1,5 +0,0 @@
-
-# A file to test dtknv in console.
-cd ..
-cd dtknv
-python3 cknv.py -r -i /home/marw/Documents/testdtknv -o /home/marw/Documents/testout 
--- test/win_argtest.bat
+++
@@ -1,6 +0,0 @@
-REM A file to test dtknv in console.
-
-cd ..
-cd dtknv
-c:\python32\python.exe cknv.py -i D:\\datastore\\tocyr\\in_files -o D:\\datastore\\tocyr\\out_files -r
-pause
